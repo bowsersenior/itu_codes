fddce467d3e394d6508eca75dfe82d358cfe63a4
Mani Tadayon <mtadayon@attinteractive.com>
2011-09-13 15:55:02 -0700

    Fix handling of Kazakhstan country code

--- lib/itu_codes.rb
+++ lib/itu_codes.rb
@@ -12,7 +12,7 @@ module ItuCodes
   # TODO: move the base data into a fixture file to speed up loading
   CODE_LIST = [
     { "0"     =>  "Reserved"},
-    
+
     { "1"     =>  "United States"},
     { "1201"  =>  "United States" },
     { "1202"  =>  "United States" },
@@ -380,8 +380,8 @@ module ItuCodes
     { "1"     =>   "American Samoa" },
     { "1684"  =>   "American Samoa" },
 
-    { "1"     =>  "Anguilla" },   
-    { "1264"  =>  "Anguilla" },   
+    { "1"     =>  "Anguilla" },
+    { "1264"  =>  "Anguilla" },
 
     { "1"     =>  "Antigua and Barbuda" },
     { "1268"  =>  "Antigua and Barbuda" },
@@ -400,7 +400,7 @@ module ItuCodes
 
     { "1"     =>  "Cayman Islands" },
     { "1345"  =>  "Cayman Islands" },
-    
+
     { "1"     =>  "Dominica" },
     { "1767"  =>  "Dominica" },
 
@@ -445,7 +445,7 @@ module ItuCodes
 
     { "1"     =>  "Virgin Islands, U.S." },
     { "1340"  =>  "Virgin Islands, U.S." },
-    
+
     {"20"     =>      "Egypt"},
     {"210"    =>      "Spare code"},
     {"211"    =>      "Spare code"},
@@ -652,9 +652,10 @@ module ItuCodes
     {"699"    =>      "Spare code"},
 
     {"7"      =>      "Russian Federation"},
+    {"7"      =>      "Kazakhstan"},
     {"76"     =>      "Kazakhstan"},
     {"77"     =>      "Kazakhstan"},
-    
+
     {"800"    =>      "International Freephone Service"},
     {"801"    =>      "Spare code"},
     {"802"    =>      "Spare code"},
@@ -757,14 +758,14 @@ module ItuCodes
   def self.list
     ItuCodes::CODE_LIST
   end
-  
+
   def self.lookup(code)
     cleaned = clean(code)
-    list.find_all{ |hsh| hsh.keys.first == cleaned }.map do |k| 
-      k.values.first 
+    list.find_all{ |hsh| hsh.keys.first == cleaned }.map do |k|
+      k.values.first
     end.uniq
   end
-  
+
   def self.find (code_or_name)
     if code_or_name.to_s.match /[^0-9]/  # non-numeric string passed
       find_by_name(code_or_name)
@@ -773,7 +774,7 @@ module ItuCodes
       find_by_itu_code(cleaned)
     end
   end
-  
+
   def self.find_by_itu_code(code)
     if american?(code) and code.length > 1
       returner = 'United States'
@@ -791,21 +792,21 @@ module ItuCodes
     end.map{ |k| k.keys.first }.uniq
     returner.size <= 1 ? returner.shift : returner
   end
-  
+
   # ie. find_by_country_iso_code('US')
   def self.iso2itu(iso_code)
-    [ItuCodes.find_by_name( Carmen::country_name(iso_code) )].flatten.reject do |c|   
+    [ItuCodes.find_by_name( Carmen::country_name(iso_code) )].flatten.reject do |c|
       north_american?(c) and c.length > 1
     end.shift rescue nil
   end
-  
+
   # this is tricky for North American destinations: '1' can be the US, Canada or another country
   def self.itu2iso(itu_code)
     ItuCodes.lookup(itu_code).map do |country|
       Carmen::country_code( country ) rescue nil
     end.compact
   end
-  
+
   # returns true for any valid ITU code
   # valid_code?(1)    => true
   # valid_code?(1818) => false
@@ -816,15 +817,15 @@ module ItuCodes
       self.list.any? { |hsh| hsh.has_key?(some_code) }
     end
   end
-  
+
   def self.north_america
     find_by_itu_code '1'
   end
-  
+
   def self.north_american?(some_code)
     some_code.to_s[0,1] == '1'
   end
-  
+
   # parse a destination code (probably with area code) to find the ITU code
   #   ex:  parse_code(1818) =>  1
   def self.parse_code(some_code)
@@ -834,7 +835,7 @@ module ItuCodes
     end
     some_code[0,sub_index] unless sub_index.nil?
   end
-  
+
   # parse a destination code (probably with area code) to find the number without the ITU code
   #   ex:  parse_number(18184443322) => 8184443322
   def self.parse_number(some_code)
@@ -843,21 +844,21 @@ module ItuCodes
       valid_code? some_code[0,len]
     end
     some_code[sub_index,some_code.length - sub_index] unless sub_index.nil?
-  end 
-  
+  end
+
   def self.compatriots?(some, other)
     both_valid = ! ( parse_code(some).nil? or parse_code(other).nil? )
     both_valid and ( parse_code(some) == parse_code(other) ) rescue nil
   end
-  
+
   def self.american?(some_code)
     # for non-US North American codes, parse_code will return a 4 digit code
     # for US, '1' will be returned
-    some_code = some_code.to_s    
+    some_code = some_code.to_s
     countries = lookup(some_code[0,4])
     north_american?(some_code) and (countries.include?('United States') or countries.empty?)
   end
-  
+
   def self.us?(some_code)
     some_code = some_code.to_s
     countries = lookup(some_code[0,4])
--- test/itu_codes_test.rb
+++ test/itu_codes_test.rb
@@ -5,7 +5,7 @@ class ItuCodesTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
-  
+
   test "calling code is valid" do
     american  =    "1"
     newyorker = "1212"
@@ -13,51 +13,51 @@ class ItuCodesTest < ActiveSupport::TestCase
     samoan    = "1684"
     russian   =    "7"
     alien     = "alksjlkknm"
-   
+
     assert ItuCodes.valid_code?(american)
     assert ItuCodes.valid_code?(russian)
-    
+
     assert ! ItuCodes.valid_code?(newyorker)     # 1212 is not a country code!
     assert ! ItuCodes.valid_code?(canadian)
-    assert ! ItuCodes.valid_code?(samoan)    
+    assert ! ItuCodes.valid_code?(samoan)
     assert ! ItuCodes.valid_code?(alien)
   end
-  
+
   test "parse full number into correct calling codes" do
     american  =    "1"
     newyorker = "1212"
     russian   =    "7"
     alien     = "alksjlkknm"
-    
+
     assert_equal american, ItuCodes.parse_code(american)
     assert_equal american, ItuCodes.parse_code(newyorker)
-   
+
     assert_not_equal russian, ItuCodes.parse_code(newyorker)
-    assert_not_equal russian, ItuCodes.parse_code(alien)    
-    
+    assert_not_equal russian, ItuCodes.parse_code(alien)
+
     assert_not_equal alien, ItuCodes.parse_code(alien)
-    
+
     assert_nil ItuCodes.parse_code(alien)
   end
-   
-   
+
+
   test "compatriot phone numbers should be detected" do
     american  =    "1"
     newyorker = "1212"
     angeleno  = "1818"
     russian   =    "7"
     alien     = "alksjlkknm"
-    
+
     assert ItuCodes.compatriots?(newyorker, angeleno)
     assert ItuCodes.compatriots?(newyorker, american)
-    
+
     assert ! ItuCodes.compatriots?(newyorker, russian)
     assert ! ItuCodes.compatriots?(american, russian)
-    
+
     assert ! ItuCodes.compatriots?(american, alien)
     assert ! ItuCodes.compatriots?(alien, alien)
   end
-  
+
   test "find should return correct result" do
     assert_equal( "995", ItuCodes.find("Georgia") )
     assert_equal( "Georgia", ItuCodes.find(995) )
@@ -65,17 +65,17 @@ class ItuCodesTest < ActiveSupport::TestCase
     assert_equal( 'United States', ItuCodes.find("12345678") )
     assert_equal( nil, ItuCodes.find("someverylongandnonterritorialstring") )
   end
-  
+
   test "should deal gracefully with North America" do
     assert_equal( "American Samoa", ItuCodes.find(1684) )
     assert_equal( "Canada"        , ItuCodes.find(1250) )
     assert_equal( "United States" , ItuCodes.find(1818) )
 
     assert ItuCodes.find('1').include?("United States")
-    assert ItuCodes.find('1').include?("Canada")    
+    assert ItuCodes.find('1').include?("Canada")
     assert ItuCodes.find('1').include?("American Samoa")
   end
-  
+
   test "should detect american numbers" do
     american  =    "1"
     newyorker = "1212"
@@ -83,12 +83,12 @@ class ItuCodesTest < ActiveSupport::TestCase
     canadian  = "1250"
     samoan    = "1684"
     russian   = "7"
-    
-    assert( ItuCodes.american?(american)    )    
+
+    assert( ItuCodes.american?(american)    )
     assert( ItuCodes.american?(newyorker)   )
-    assert( ItuCodes.american?(n2)          )    
-    assert( ! ItuCodes.american?(canadian)  )    
-    assert( ! ItuCodes.american?(samoan)    )        
+    assert( ItuCodes.american?(n2)          )
+    assert( ! ItuCodes.american?(canadian)  )
+    assert( ! ItuCodes.american?(samoan)    )
     assert( ! ItuCodes.american?(russian)   )
   end
 
@@ -102,15 +102,16 @@ class ItuCodesTest < ActiveSupport::TestCase
 
   test "should convert from ISO 2-letter code to ITU code" do
     assert_equal( '1',  ItuCodes.iso2itu('US') )
-    assert_equal( '1',  ItuCodes.iso2itu('CA') )    
-    assert_nil( ItuCodes.iso2itu('not_an_iso_code') )        
+    assert_equal( '1',  ItuCodes.iso2itu('CA') )
+    assert_nil( ItuCodes.iso2itu('not_an_iso_code') )
     assert_equal( '52', ItuCodes.iso2itu('MX') )
   end
 
   test "should convert from ITU code to ISO 2-letter code" do
-    assert( ItuCodes.itu2iso('1').include?('US') )    
+    assert( ItuCodes.itu2iso('1').include?('US') )
     assert( ItuCodes.itu2iso('7').include?('RU') )
-    assert( ItuCodes.itu2iso('7').include?('KZ') )    
-    assert( ItuCodes.itu2iso('995').include?('GE') )    
+    assert( ItuCodes.itu2iso('7').include?('KZ') )
+    assert( ItuCodes.itu2iso('76').include?('KZ') )
+    assert( ItuCodes.itu2iso('995').include?('GE') )
   end
 end
